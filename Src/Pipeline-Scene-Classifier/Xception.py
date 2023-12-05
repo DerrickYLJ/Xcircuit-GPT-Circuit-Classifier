21d2e215aacc992106e0b2b2296e720878c965b4
DerrickYLJ <lijiey@andrew.cmu.edu>
2023-12-04 22:51:12 -0500

    Xception incorporation

---
+++ Src/Pipeline-Scene-Classifier/Xception.py
@@ -0,0 +1,112 @@
+import numpy as np
+import keras
+from keras import layers
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+tfds.disable_progress_bar()
+# Getting the data
+train_ds, validation_ds = keras.utils.image_dataset_from_directory(directory = "../../data/Circuits/Xception_Train/", validation_split = 0.2, seed = 1, batch_size=16, subset = "both")
+test_ds = keras.utils.image_dataset_from_directory(directory = "../../data/Circuits/Xception_Test/")
+print(f"Number of training samples: {train_ds.cardinality()}")
+print(f"Number of validation samples: {validation_ds.cardinality()}")
+
+# plt.figure(figsize=(10, 10))
+# for i, (image, label) in enumerate(train_ds.take(9)):
+#     ax = plt.subplot(3, 3, i + 1)
+#     plt.imshow(image)
+#     plt.title(int(label))
+#     plt.axis("off")
+
+# Resizing the data
+resize_fn = keras.layers.Resizing(150, 150)
+train_ds = train_ds.map(lambda x, y: (resize_fn(x), y))
+validation_ds = validation_ds.map(lambda x, y: (resize_fn(x), y))
+test_ds = test_ds.map(lambda x, y: (resize_fn(x), y))
+
+
+# Data Augmentation
+augmentation_layers = [
+    layers.RandomFlip("horizontal"),
+    layers.RandomRotation(0.1),
+]
+
+def data_augmentation(x):
+    for layer in augmentation_layers:
+        x = layer(x)
+    return x
+
+train_ds = train_ds.map(lambda x, y: (data_augmentation(x), y))
+
+# prefetching and normalizing the data
+from tensorflow import data as tf_data
+
+train_ds = train_ds.prefetch(tf_data.AUTOTUNE).cache()
+validation_ds = validation_ds.prefetch(tf_data.AUTOTUNE).cache()
+
+
+# loading the model
+base_model = keras.applications.Xception(
+    weights="imagenet",  # Load weights pre-trained on ImageNet.
+    input_shape=(150, 150, 3),
+    include_top=False,
+)  # Do not include the ImageNet classifier at the top.
+
+# Freeze the base_model
+base_model.trainable = False
+
+# Create new model on top
+inputs = keras.Input(shape=(150, 150, 3))
+
+# Pre-trained Xception weights requires that input be scaled
+# from (0, 255) to a range of (-1., +1.), the rescaling layer
+# outputs: `(inputs * scale) + offset`
+scale_layer = keras.layers.Rescaling(scale=1 / 127.5, offset=-1)
+x = scale_layer(inputs)
+
+# The base model contains batchnorm layers. We want to keep them in inference mode
+# when we unfreeze the base model for fine-tuning, so we make sure that the
+# base_model is running in inference mode here.
+x = base_model(x, training=False)
+x = keras.layers.GlobalAveragePooling2D()(x)
+x = keras.layers.Dropout(0.2)(x)  # Regularize with dropout
+outputs = keras.layers.Dense(1)(x)
+model = keras.Model(inputs, outputs)
+
+model.summary(show_trainable=True)
+
+model.compile(
+    optimizer=keras.optimizers.legacy.Adam(),
+    loss=keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=[keras.metrics.BinaryAccuracy()],
+)
+
+epochs = 2
+print("Fitting the top layer of the model")
+model.fit(train_ds, epochs=epochs, validation_data=validation_ds)
+
+# Round of fine-tuning 
+# Unfreeze the base_model. Note that it keeps running in inference mode
+# since we passed `training=False` when calling it. This means that
+# the batchnorm layers will not update their batch statistics.
+# This prevents the batchnorm layers from undoing all the training
+# we've done so far.
+base_model.trainable = True
+model.summary(show_trainable=True)
+
+model.compile(
+    optimizer=keras.optimizers.legacy.Adam(1e-5),  # Low learning rate
+    loss=keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=[keras.metrics.BinaryAccuracy()],
+)
+
+epochs = 1
+print("Fitting the end-to-end model")
+model.fit(train_ds, epochs=epochs, validation_data=validation_ds)
+
+print("Test dataset evaluation")
+model.evaluate(test_ds)
